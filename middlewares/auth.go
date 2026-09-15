f292c6d6c07afb695b28f06f6f72f3b4cc6bf289
maii0710 <maicoconut919@gmail.com>
2026-09-15 09:13:16 +0700

    perubahan auth.go dan go.mod

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,30 +1,28 @@
 package middlewares
 
 import (
+	"belajar_go/utils" 
 	"net/http"
 	"strings"
-
-	"github.com/golang-jwt/jwt/v5"
 )
-
-var SecretKey = []byte("kunci_rahasia_kamu") // Sesuaikan key JWT kamu
 
 func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			http.Error(w, `{"message": "Akses ditolak, token tidak ditemukan"}`, http.StatusUnauthorized)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(`{"message": "Akses ditolak, token tidak ditemukan"}`))
 			return
 		}
 
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
 
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			return SecretKey, nil
-		})
-
-		if err != nil || !token.Valid {
-			http.Error(w, `{"message": "Token tidak valid atau kadaluarsa"}`, http.StatusUnauthorized)
+		_, err := utils.ValidateToken(tokenString)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte(`{"message": "Token tidak valid atau kadaluarsa"}`))
 			return
 		}
 
